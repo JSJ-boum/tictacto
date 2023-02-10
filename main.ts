c2d70410bde2051a12506dd8f11ebe932d7e1aab
JSJ-boum <123979554+JSJ-boum@users.noreply.github.com>
2023-02-10 11:50:04 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,40 @@
 input.onButtonPressed(Button.A, function () {
     Compter += 1
     if (Compter == 1) {
-        led.plotBrightness(0, 0, 100)
+        led.plotBrightness(0, 0, 23)
+        led.unplot(4, 4)
     } else {
         if (Compter == 2) {
-            led.plotBrightness(2, 0, 100)
+            led.plotBrightness(2, 0, 23)
+            led.unplot(0, 0)
         } else {
             if (Compter == 3) {
-                led.plotBrightness(4, 0, 100)
+                led.plotBrightness(4, 0, 23)
+                led.unplot(2, 0)
             } else {
                 if (Compter == 4) {
-                    led.plotBrightness(0, 2, 100)
+                    led.plotBrightness(0, 2, 23)
+                    led.unplot(4, 0)
                 } else {
                     if (Compter == 5) {
-                        led.plotBrightness(2, 2, 100)
+                        led.plotBrightness(2, 2, 23)
+                        led.unplot(0, 2)
                     } else {
                         if (Compter == 6) {
-                            led.plotBrightness(4, 2, 100)
+                            led.plotBrightness(4, 2, 23)
+                            led.unplot(2, 2)
                         } else {
                             if (Compter == 7) {
-                                led.plotBrightness(0, 4, 100)
+                                led.plotBrightness(0, 4, 23)
+                                led.unplot(4, 2)
                             } else {
                                 if (Compter == 8) {
-                                    led.plotBrightness(2, 4, 100)
+                                    led.plotBrightness(2, 4, 23)
+                                    led.unplot(0, 4)
                                 } else {
                                     if (Compter == 9) {
-                                        led.plotBrightness(4, 4, 100)
+                                        led.plotBrightness(4, 4, 23)
+                                        led.unplot(2, 4)
                                     }
                                 }
                             }
@@ -36,8 +45,59 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.B, function () {
+    compter_2 += 1
+    if (compter_2 == 1) {
+        led.plotBrightness(0, 0, 100)
+        led.unplot(4, 4)
+    } else {
+        if (compter_2 == 2) {
+            led.plotBrightness(2, 0, 100)
+            led.unplot(0, 0)
+        } else {
+            if (compter_2 == 3) {
+                led.plotBrightness(4, 0, 100)
+                led.unplot(2, 0)
+            } else {
+                if (compter_2 == 4) {
+                    led.plotBrightness(0, 2, 100)
+                    led.unplot(4, 0)
+                } else {
+                    if (compter_2 == 5) {
+                        led.plotBrightness(2, 2, 100)
+                        led.unplot(0, 2)
+                    } else {
+                        if (compter_2 == 6) {
+                            led.plotBrightness(4, 2, 100)
+                            led.unplot(2, 2)
+                        } else {
+                            if (compter_2 == 7) {
+                                led.plotBrightness(0, 4, 100)
+                                led.unplot(4, 2)
+                            } else {
+                                if (compter_2 == 8) {
+                                    led.plotBrightness(2, 4, 100)
+                                    led.unplot(0, 4)
+                                } else {
+                                    if (compter_2 == 9) {
+                                        led.plotBrightness(4, 4, 100)
+                                        led.unplot(2, 4)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+let compter_2 = 0
 let Compter = 0
 Compter = 0
+basic.forever(function () {
+	
+})
 basic.forever(function () {
     led.plotBrightness(1, 0, 255)
     led.plotBrightness(1, 1, 255)
